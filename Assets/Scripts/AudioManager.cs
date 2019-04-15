6ec67703d2807a9e2e0fa18799195e153d2462b8
Rafaela0Cruz <rafaelap.cruz0@gmail.com>
2019-04-15 10:53:06 -0300

    Tocando o som da moeda com AudioManager e AusdioSFX,aula08

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+   public static AudioManager instance;
+
+public AudioSFX audioSFX;
+
+   void Awake() {
+       if (instance == null){
+           instance = this;
+       }
+   }
+
+   public void PlaySoudCoinsPickup(GameObject obj){
+       AudioSource.PlayClipAtPoint(audioSFX.coinPichup, obj.transform.position);
+   }
+}
